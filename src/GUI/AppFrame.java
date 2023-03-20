c00fabe407e9b4aaf6da86d55800697b3f08cda4
Utkarsh Shukla <63734211+Utkarsh-Technical@users.noreply.github.com>
2023-03-20 19:54:10 +0530

    Upadate the UI

--- src/GUI/AppFrame.java
+++ src/GUI/AppFrame.java
@@ -13,58 +13,70 @@ import javax.swing.JOptionPane;
 import comp_decomp.compressor;
 import comp_decomp.decompressor;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 
 public class AppFrame extends JFrame implements ActionListener {
-	
-	JButton compressButton;
-	JButton decompressButton;
-	
-	AppFrame(){
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		compressButton = new JButton("Select file to compress");
-		compressButton.setBounds(20, 100, 200, 30);
-		compressButton.addActionListener(this);
-		
-		decompressButton = new JButton("Select file to decompress");
-		decompressButton.setBounds(250, 100, 200, 30);
-		decompressButton.addActionListener(this);
-		
-		this.add(compressButton);
-		this.add(decompressButton);
-		this.setSize(1000, 500);
-		this.getContentPane().setBackground(Color.red);
-		this.setVisible(true);
-		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == compressButton) {
-			JFileChooser filechooser = new JFileChooser();
-			int response = filechooser.showSaveDialog(null);
-			if(response == JFileChooser.APPROVE_OPTION) {
-				File file = new File(filechooser.getSelectedFile().getAbsolutePath());
-				System.out.println(file);
-				try {
-					compressor.method(file);
-				}catch(Exception ee) {
-					JOptionPane.showMessageDialog(null,ee.toString());
-				}
-			}
-		}
-		if(e.getSource() == decompressButton) {
-			JFileChooser filechooser = new JFileChooser();
-			int response = filechooser.showSaveDialog(null);
-			if(response == JFileChooser.APPROVE_OPTION) {
-				File file = new File(filechooser.getSelectedFile().getAbsolutePath());
-				System.out.println(file);
-				try {
-					decompressor.method(file);
-				}catch(Exception ee) {
-					JOptionPane.showMessageDialog(null,ee.toString());
-				}
-			}
-		}
-	}
+
+    JButton compressButton;
+    JButton decompressButton;
+    
+    AppFrame(){
+    	this.setTitle("Compressor and Decompressor");
+        this.setSize(800,400);
+        this.setLocation(400,200);
+
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        compressButton = new JButton("SELECT TO COMPRESS");
+        compressButton.setBounds(80,100,200,50);
+        compressButton.addActionListener(this);
+
+        decompressButton = new JButton("SELECT TO DECOMPRESS");
+        decompressButton.setBounds(500,100,200,50);
+        decompressButton.addActionListener(this);
+
+
+        this.add(compressButton);
+        this.add(decompressButton);
+        this.getContentPane().setBackground(Color.decode("#E3B778"));
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == compressButton){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println("Compresser "+file);
+                try{
+                    compressor.method(file);
+                }
+                catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.toString());
+                }
+            }
+        }
+
+        if(e.getSource() == decompressButton){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println("DeCompresser "+file);
+                try{
+                    decompressor.method(file);
+                }
+                catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.toString());
+                }
+            }
+        }
+    }
 }
